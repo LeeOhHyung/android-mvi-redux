c0d78b699c3eba92d7fafc299b7c67f9e7e90772
LeeOhHyung <lee2928030@naver.com>
2020-09-20 21:31:33 +0900

    feat: Network Exception Handling 을 위한 Exception sealed class를 domain, data layer에 추가

--- remote/src/main/java/kr/ohyung/remote/source/PhotoRemoteDataSourceImpl.kt
+++ remote/src/main/java/kr/ohyung/remote/source/PhotoRemoteDataSourceImpl.kt
@@ -4,10 +4,12 @@
 package kr.ohyung.remote.source
 
 import io.reactivex.Single
+import kr.ohyung.data.exception.NetworkException
 import kr.ohyung.data.model.PhotoSummaryDataModel
 import kr.ohyung.data.source.remote.PhotoRemoteDataSource
 import kr.ohyung.remote.api.PhotosApi
 import kr.ohyung.remote.mapper.PhotosResponseMapper
+import retrofit2.HttpException
 
 class PhotoRemoteDataSourceImpl(
     private val photosApi: PhotosApi,
@@ -16,4 +18,10 @@ class PhotoRemoteDataSourceImpl(
     override fun getPhotos(page: Int, perPage: Int, orderBy: String): Single<List<PhotoSummaryDataModel>> =
         photosApi.getPhotos(page, perPage, orderBy)
             .map { response -> photosResponseMapper.toDataModels(response) }
+            .onErrorResumeNext { throwable ->
+                when(throwable) {
+                    is HttpException -> Single.error(NetworkException.BadRequestException(throwable.message.toString()))
+                    else -> Single.error(throwable)
+                }
+            }
 }
