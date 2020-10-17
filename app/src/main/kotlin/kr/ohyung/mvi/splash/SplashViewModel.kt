32e63f2cc716eb66fbd9ae83bb590ac29c5e39ea
LeeOhHyung <lee2928030@naver.com>
2020-10-17 18:40:55 +0900

    feat: BaseFragment, BaseViewModel을 상속받는 MviFragment, MviViewModel로 상속구조 변경

--- app/src/main/kotlin/kr/ohyung/mvi/splash/SplashViewModel.kt
+++ app/src/main/kotlin/kr/ohyung/mvi/splash/SplashViewModel.kt
@@ -4,12 +4,12 @@
 package kr.ohyung.mvi.splash
 
 import androidx.hilt.lifecycle.ViewModelInject
-import kr.ohyung.core.android.BaseViewModel
+import kr.ohyung.core.android.MviViewModel
 import kr.ohyung.mvi.splash.mvi.*
 
 class SplashViewModel @ViewModelInject constructor(
     private val splashStateMachine: SplashStateMachine
-) : BaseViewModel<SplashViewIntent, SplashViewAction, SplashViewState, SplashViewResult>(splashStateMachine) {
+) : MviViewModel<SplashViewIntent, SplashViewAction, SplashViewState, SplashViewResult>(splashStateMachine) {
 
     override val viewState
         get() = splashStateMachine.currentState
